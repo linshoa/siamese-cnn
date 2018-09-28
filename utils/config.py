b6b0ce0419469ab2af12b74fec227123003d38ad
lsc <657477836@qq.com>
2018-09-28 16:30:47 +0800

    update train and evaluate

--- utils/config.py
+++ utils/config.py
@@ -1,6 +1,6 @@
 DukeMTMC_name_dir ='./data/DukeMTMC/'
 DukeMTMC_img_dir = '/home/ubuntu/media/File/1Various/Person_reid_dataset/DukeMTMC-reID/'
-BATCH_SIZE = 128
+BATCH_SIZE = 64
 TARGET_SIZE = [224, 80]
 EPOCH = 10000
 SAVE_ITER = 50
